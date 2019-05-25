4ac218bbd60bf943288366dc2be7c99008380089
uksp99 <uksp99@gmail.com>
2019-05-25 23:07:29 +0530

    Updatedlibraryfolder

--- API_Automation/ReadContent/ReadJsonContent.py
+++ API_Automation/ReadContent/ReadJsonContent.py
@@ -1,7 +1,7 @@
 import json
 
 def read_request_content():
-    file=  open('C:/Z-Udhayakumar/IOT/Automation_Projects/InputData/Request.json','r')
+    file=  open('C:/Z-Udhayakumar/Udhayakumar/APIAutomationRepository/API_Automation/InputData/Request.json','r')
     jsonfile=file.read()
     json_content=json.loads(jsonfile)
     return json_content
